c38bc4f92947e7e1b69d2fc8aa847711292d2a9c
Hyormone <ddf998@gmail.com>
2026-06-09 14:25:02 +0900

    프로젝트 기본 토대 설정

---
+++ hire-radar-backend/src/main/java/com/hyome/hire_radar_backend/HireRadarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hyome.hire_radar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireRadarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireRadarBackendApplication.class, args);
+	}
+
+}
---
+++ hire-radar-backend/src/test/java/com/hyome/hire_radar_backend/HireRadarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyome.hire_radar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireRadarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
